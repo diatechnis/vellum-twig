ff28d5592542d27f5b95b793ca0126cf46d68cfa
Michael Kelly <puritandesigns@gmail.com>
2019-03-03 13:42:51 -0500

    Check for FetchDataInterface in TwigRenderer

--- src/VellumTwig/TwigRenderer.php
+++ src/VellumTwig/TwigRenderer.php
@@ -3,6 +3,7 @@
 namespace VellumTwig;
 
 use Vellum\Contracts\Components\ComponentInterface;
+use Vellum\Contracts\FetchDataInterface;
 use Vellum\Path\TemplatePathInterface;
 
 class TwigRenderer implements \Vellum\Contracts\Renderers\RenderInterface
@@ -24,13 +25,17 @@ class TwigRenderer implements \Vellum\Contracts\Renderers\RenderInterface
     {
         $arguments = $component->getArguments();
 
+        $data = ['arguments' => $arguments];
+
+        if ($component instanceof FetchDataInterface) {
+            $data = array_merge($data, $component->getData());
+        }
+
         $template_path = $this->template_path_resolver->resolve(
             $component,
             false
         );
 
-        return $this->twig->render($template_path, [
-            'arguments' => $arguments
-        ]);
+        return $this->twig->render($template_path, $data);
     }
 }
